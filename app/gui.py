5516f5293bdc00ee2acb377c67315b1a6010a0fb
nothumancollective <nothumancollective@users.noreply.github.com>
2026-02-14 01:49:47 +0100

    fix: use maximized windows to keep native titlebar controls

--- app/gui.py
+++ app/gui.py
@@ -1212,22 +1212,24 @@ class GuiController:
 
     def show_project_manager(self) -> None:
         self.project_manager.refresh()
-        self.project_manager.showFullScreen()
+        self.project_manager.setWindowState(self.project_manager.windowState() & ~Qt.WindowFullScreen)
+        self.project_manager.showMaximized()
         apply_windows_dark_titlebar(self.project_manager)
 
-    def _show_main_window_fullscreen(self) -> None:
-        self.main_window.showFullScreen()
+    def _show_main_window_maximized(self) -> None:
+        self.main_window.setWindowState(self.main_window.windowState() & ~Qt.WindowFullScreen)
+        self.main_window.showMaximized()
         apply_windows_dark_titlebar(self.main_window)
 
     def _open_project(self, project_id: str) -> None:
         project = self.service.repo.load_project(project_id)
         self.main_window.load_project(project)
-        self._show_main_window_fullscreen()
+        self._show_main_window_maximized()
         self.project_manager.hide()
 
     def _new_project(self) -> None:
         self.main_window.current_project = None
-        self._show_main_window_fullscreen()
+        self._show_main_window_maximized()
         self.main_window.show_project()
         self.project_manager.hide()
 
